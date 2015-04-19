97817fe6dba3073ed04c56c4131c27c4e7f1c2a5
niinyarko <niinyarko@yahoo.com>
2015-04-19 15:38:00 +0000

    added new meteor methods

--- server/allows.js
+++ server/allows.js
@@ -1,13 +1,7 @@
 /*Posts.allow({
-    insert: function (userId, doc) {
-        return (userId);
-    },
     update: function (userId, doc, fields, modifier) {
         // return (userId && fields[0] === "score" || fields[0] === "upvoters" || userId === doc.owner);
         return userId && _.without(_.keys(doc), 'score').length === 0 || doc.owner === userId;
-    },
-    remove: function (userId, doc) {
-        return (userId && (userId === doc.owner));
     }
 });*/
 
